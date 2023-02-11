a56148bfc8fbe78092c0416f420eaf0b0adfc75c
eduardosdl <edu.silva.e08@gmail.com>
2023-02-11 11:08:39 -0300

    check if UUID is valid

---
+++ server/src/app/utils/isNotValidUUID.js
@@ -0,0 +1,7 @@
+function isNotValidUUID(string) {
+  const regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+  return regex.test(string);
+}
+
+module.exports = isNotValidUUID;
