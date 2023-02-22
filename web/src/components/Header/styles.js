7a531595aafca2c480e7f2e4f51d5f04f40c515a
eduardosdl <edu.silva.e08@gmail.com>
2023-02-22 12:39:44 -0300

    header styles

---
+++ web/src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 24px;
+`;
