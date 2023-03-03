25ceff4e859b85fd0d42da17676a315426e502d4
eduardosdl <edu.silva.e08@gmail.com>
2023-03-03 13:33:59 -0300

    create a client http to centralize rules

---
+++ web/src/services/utils/HttpClient.js
@@ -0,0 +1,55 @@
+import APIError from '../../errors/APIError';
+
+export default class HttpClinet {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async makeRequest(path, options) {
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
+
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: options.method,
+      body: JSON.stringify(options.body),
+      headers,
+    });
+
+    let responseBody = null;
+    const contentType = response.headers.get('content-type');
+
+    if (contentType.includes('application/json')) {
+      responseBody = await response.json();
+    }
+
+    if (response.ok) {
+      return responseBody;
+    }
+
+    throw new APIError(response, responseBody);
+  }
+
+  get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
+  }
+
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+}
