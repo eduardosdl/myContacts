262a17b90e79056a66f46b7119371493841c4377
eduardosdl <edu.silva.e08@gmail.com>
2023-03-06 20:51:55 -0300

    contact form component

---
+++ web/src/components/ContactForm/index.jsx
@@ -0,0 +1,146 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../hooks/useErrors';
+import CategoriesService from '../../services/CategoriesService';
+
+import FormGroup from '../FormGroup';
+import Button from '../Button';
+import Input from '../Input';
+import Select from '../Select';
+
+import { Form, ButtonContainer } from './styles';
+
+export default function ContactForm({ buttonLabel, onSubmit }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [isLoadCategories, setIsLoadCategories] = useState(true);
+  const [isSubmiting, setIsSubmiting] = useState(false);
+
+  const {
+    errors, setError, removeError, getErrorMessageByFieldName,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
+
+  useEffect(() => {
+    async function loadCategories() {
+      try {
+        const categoriesList = await CategoriesService.listCategories();
+
+        setCategories(categoriesList);
+      } catch {} finally {
+        setIsLoadCategories(false);
+      }
+    }
+
+    loadCategories();
+  }, []);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError({ field: 'name' });
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido' });
+    } else {
+      removeError({ field: 'email' });
+    }
+  }
+
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsSubmiting(true);
+
+    await onSubmit({
+      name, email, phone, categoryId,
+    });
+
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategories([]);
+
+    setIsSubmiting(false);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          placeholder="Nome *"
+          value={name}
+          onChange={handleNameChange}
+          disabled={isSubmiting}
+        />
+      </FormGroup>
+
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          disabled={isSubmiting}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+          disabled={isSubmiting}
+        />
+      </FormGroup>
+
+      <FormGroup isLoading={isLoadCategories}>
+        <Select
+          value={categoryId}
+          onChange={(event) => setCategoryId(event.target.value)}
+          disabled={isLoadCategories || isSubmiting}
+        >
+          <option value="">Sem categoria</option>
+
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </Select>
+      </FormGroup>
+      <ButtonContainer>
+        <Button type="submit" disabled={!isFormValid} isLoading={isSubmiting}>
+          {buttonLabel}
+        </Button>
+      </ButtonContainer>
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
