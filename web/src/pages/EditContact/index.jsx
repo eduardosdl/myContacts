0f92362af14b490f5ceecf4aebd4a4974197132e
eduardosdl <edu.silva.e08@gmail.com>
2023-03-21 10:15:38 -0300

    add edit contact page

---
+++ web/src/pages/EditContact/index.jsx
@@ -0,0 +1,11 @@
+import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
+
+export default function NewContact() {
+  return (
+    <>
+      <PageHeader title="Editar Eduardo Silva" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
+  );
+}
