7cecb55e1ea3461df7c4617fbe8da33802227ba7
eduardosdl <edu.silva.e08@gmail.com>
2023-05-03 17:59:32 -0300

    add and remove event listners

--- web/src/components/Toast/ToastContainer/index.jsx
+++ web/src/components/Toast/ToastContainer/index.jsx
@@ -2,29 +2,33 @@ import { useEffect, useState } from 'react';
 
 import ToastMessage from '../ToastMessage';
 
+import { toastEventManager } from '../../../utils/toast';
+
 import { Container } from './styles';
 
 export default function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
-
+    function handleAddToast({ type, text }) {
       setMessages((prevState) => [...prevState, { id: Math.random(), type, text }]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-      document.removeEventListener('addtoast', handleAddToast);
+      toastEventManager.removeListener('addtoast', handleAddToast);
     };
   }, []);
+
+  function handleRemoveMessage(id) {
+    setMessages((prevState) => prevState.filter((message) => message.id !== id));
+  }
 
   return (
     <Container>
       {messages.map((message) => (
-        <ToastMessage key={message.id} type={message.type} text={message.text} />
+        <ToastMessage key={message.id} message={message} onRemoveMessage={handleRemoveMessage} />
       ))}
     </Container>
   );
