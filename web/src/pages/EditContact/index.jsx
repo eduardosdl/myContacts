206e6cbcd6abc7f99f86d7183e8569d35611ca57
eduardosdl <edu.silva.e08@gmail.com>
2023-05-11 09:39:51 -0300

    load contacts in form

--- web/src/pages/EditContact/index.jsx
+++ web/src/pages/EditContact/index.jsx
@@ -1,11 +1,45 @@
+import { useHistory, useParams } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 
 export default function NewContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFieldsValues(contact);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({ type: 'danger', text: 'Contato não encontrado' });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Eduardo Silva" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm ref={contactFormRef} buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
