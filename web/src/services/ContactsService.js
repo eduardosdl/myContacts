89c9375915aa7af87bd6997f46a383f696755b75
Eduardo Silva <edu.silva.e08@gmail.com>
2023-12-07 12:34:04 -0300

    update api url

--- web/src/services/ContactsService.js
+++ web/src/services/ContactsService.js
@@ -2,7 +2,7 @@ import HttpClient from './utils/HttpClient';
 
 class ContactsService {
   constructor() {
-    this.httpClient = new HttpClient('http://localhost:3000');
+    this.httpClient = new HttpClient('https://mycontacts-bub07ti1.b4a.run');
   }
 
   listContacts(orderBy = 'asc') {
